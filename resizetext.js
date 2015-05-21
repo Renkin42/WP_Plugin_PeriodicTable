49c414c5de87dcc7226d348c883052481e2d5342
Renkin42 <arleydecker@renkin42.net>
2015-05-21 04:15:55 -0600

    Fix slightly broken jQuery function...

--- resizetext.js
+++ resizetext.js
@@ -4,10 +4,16 @@
  * and open the template in the editor.
  */
 
-jQuery(".periodic-table").each(function(index, element) {
-    var textSize = 100;
-    while (jQuery(element).width() > jQuery(element).parent().width()) {
-        textSize -= 1;
-        jQuery(element).css('font-size', textSize);
-    }
-});
+jQuery(window).load(resize_table);
+
+jQuery(window).resize(resize_table);
+
+function resize_table() {
+    jQuery(".periodic-table").each(function (index, element) {
+        var textSize = 100;
+        do {
+            jQuery(element).css('font-size', textSize);
+            textSize--;
+        } while (jQuery(element).width() > jQuery(element).parent().width())
+    });
+}
