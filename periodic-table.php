062b00041108770eca807fb7bb7af3166ca6c45c
Renkin42 <arleydecker@renkin42.net>
2015-05-21 18:24:31 -0600

    Speed vs accuracy, quite the conundrum. Oh yeah, tidied up the license headers.

--- periodic-table.php
+++ periodic-table.php
@@ -8,14 +8,12 @@
   Author: Renkin42
   Author URI:
   License: MPL 2.0
- */
 
-/*
- * Copyright (C) 2015 Austin Leydecker
- * 
- * This Source Code Form is subject to the terms of the Mozilla Public License, 
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can 
- * obtain one at http://mozilla.org/MPL/2.0/.
+  Copyright (C) 2015 Austin Leydecker
+  
+  This Source Code Form is subject to the terms of the Mozilla Public License, 
+  v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+  obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 $search = array('%nameupper%', '%namelower%', '%symbolupper%', '%symbollower%', '%number%', '%0number%' , '%group%');
